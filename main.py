e4f38aeda593e80d8fb238efd7b4da97aa507b20
1999shame <shameer.dimmagudi1999@gmail.com>
2022-05-07 09:46:12 -0700

    some comments added

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+#importing the Random and Datetime Modules
 import random
 import datetime as dt
-
+#Writing the Guessingno fun in that all the main stuff is present
 def Guessingno():
     Name = input('ENTER YOUR NAME:')
     Sex = input('ENTER YOUR GENDER:')
     Age = int(input('ENTER YOUR AGE:'))
+    # Getting choice on play game from the user
     choice = input('HELLO '+ Name.upper() +' ARE YOU READY NOW?')
     if choice.upper() == 'YES':
         print('''OK THATS !!! GREAT\n
         ____________________\n
         ____________________\n
         ____________________''')
+        #using the Random module method randrange to generate a random number
         Guessno = random.randrange(1,50)
         print('HEY '+ Name.upper() +' YOU HAVE FIVE CHANCES TO GUESS THE NUMBER IF YOU NOT GUESSED CORRECTLY THE GAME WILL BE ENDED.')
         print('HEY '+ Name.upper() + ' WE ARE GIVING A GAME STARTED POINTS TO YOU I.E IS 10')
+        # using a while loop for the chances
         points = 10
         i = 1
         while i<=5:
             userguessing = int(input('HEY!! '+ Name.upper() +' GUESS THE NUMBER IN BETWEEN 1 AND 50:'))
+            # checking the condition
             if userguessing == Guessno:
                 print('HEY!! '+ Name.upper() +' HURRY YOU GUESSED CORRECTLY.')
                 points = points + 5
@@ -41,7 +46,7 @@ def Guessingno():
     else:
         print('OK PLAY THE GAME AFTER SOMETIME ')
 
-
+# knowing the interest of the user to play the game or not
 while True:
     x = dt.datetime.now()
     print('NOW THE TIME IS ',x)
